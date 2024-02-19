7f53620e740f5917d49ba429e270e654c87fa3a8
Rabia-Rajput <rajput.rabia@outlook.com>
2024-02-19 18:44:53 +0100

    code added to config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,12 @@
 
+function openPlayerConfig(){
+
+    playerConfigOverlay.style.display = 'block'; 
+    backdropElement.style.display = 'block';
+
+}
+function closePlayerConfig(){
+    playerConfigOverlay.style.display = 'none'; 
+    backdropElement.style.display = 'none';
+
+}
